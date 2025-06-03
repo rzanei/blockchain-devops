bd6a1747c8462e35623639c80a5967d14ef58bd2
rzanei <dev.rzanei@gmail.com>
2025-06-03 21:18:45 +0100

    feat: created akash service for instance run ans stop safely

---
+++ akash_setup/akash-service-run.sh
@@ -0,0 +1,13 @@
+    #!/bin/bash
+set -e
+
+echo "🚀 Starting Akash Validator..."
+
+# Trap clean shutdown
+trap 'echo "🛑 SIGTERM received, stopping..."; kill -TERM "$child"; wait "$child"; exit 0' SIGTERM
+
+# Start the node process
+/usr/local/bin/akash_start.sh &
+
+child=$!
+wait "$child"
